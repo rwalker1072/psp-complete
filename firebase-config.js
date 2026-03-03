76c98f0ec953077dd485a5fc7ae61ac8c43ac79f
Robert Walker <rwalker1072@hotmail.com>
2026-03-02 18:18:25 -0800

    separated firebase config

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyA4TeoMnRf36oC2ZSRqS2FcUkzzpIT5Cpg",
+  authDomain: "psp-complete.firebaseapp.com",
+  projectId: "psp-complete",
+  storageBucket: "psp-complete.firebasestorage.app",
+  messagingSenderId: "640678360141",
+  appId: "1:640678360141:web:074169f4c88f2097a45804",
+  measurementId: "G-05ELN84PF5"
+};
+
+window.firebaseConfig = firebaseConfig;
